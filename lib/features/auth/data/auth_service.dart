6ef92d842eb9402c2b4468453fca8b874ad0612a
Alifnewjourney Jovani safik <furqonseli@gmail.com>
2025-10-29 15:39:43 +0700

    Add auth service

---
+++ lib/features/auth/data/auth_service.dart
@@ -0,0 +1,53 @@
+
+import 'package:outfit_store/models/user_model.dart';
+import '../../../../../services/api_client.dart';
+import '../../../../../services/storage_service.dart';
+
+class AuthService {
+  final ApiClient _appClient = ApiClient();
+  final StorageService _storage = StorageService();
+
+  //Signup
+  Future<User> signUp(String firstName, String email, String password) async {
+    final response = await _appClient.post('/api/auth/signup', {
+      'firstName': firstName,
+      'email': email,
+      'password': password,
+    });
+    final userJson = response.data['user'];
+    final token = response.data['token'];
+
+    //save token locally
+    await _storage.saveToken(token);
+    return User.fromJson(userJson);
+  }
+
+  //Login
+  Future<User> login(String email, String password) async {
+    final response = await _appClient.post("/api/auth/login", {
+      'email': email,
+      'password': password,
+    });
+
+    final userJson = response.data['user'];
+    final token = response.data['token'];
+    // Save token locally
+    await _storage.saveToken(token);
+
+    return User.fromJson(userJson);
+  }
+
+  //Get current user from token
+  Future<User?> getProfile() async {
+    final token = await _storage.getToken();
+    if (token == null) return null;
+
+    final response = await _appClient.get('/api/auth/profile');
+    return User.fromJson(response.data['user']);
+  }
+
+  //Logout
+  Future<void> logout() async {
+    await _storage.clearToken();
+  }
+}
