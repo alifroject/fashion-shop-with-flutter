febfd5637a26ea76a53cbbecbb75ff92d20fabc3
Alifnewjourney Jovani safik <furqonseli@gmail.com>
2025-10-30 17:47:29 +0700

    Add responsive spacing

---
+++ lib/core/utils/spacing.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+class Space {
+  static Widget v(double height) => SizedBox(height: height);
+  static Widget h(double width) => SizedBox(width: width);
+}
