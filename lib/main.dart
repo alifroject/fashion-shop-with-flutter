5071f5d8e10ebc7f8af63291b4c9b0cb1c99d731
Alifnewjourney Jovani safik <furqonseli@gmail.com>
2025-10-30 17:50:22 +0700

    Manual visual run for each page

--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,47 @@
+// ---------------------- FULL APP SETUP ----------------------
+// import 'package:flutter/material.dart';
+// import 'package:provider/provider.dart';
+// import 'features/auth/provider/auth_provider.dart'; // widget is dealing with provider while providing is communicating with service
+// import 'routes/router.dart';
+// import 'core/theme.dart';
+
+// void main() {
+//   runApp(
+//     MultiProvider(
+//       providers: [ChangeNotifierProvider(create: (_) => AuthProvider())],
+//       child: const MyApp(),
+//     ),
+//   );
+// }
+
+// class MyApp extends StatelessWidget {
+//   const MyApp({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       title: 'Outfit Store',
+//       theme: AppTheme.lightTheme,
+//       darkTheme: AppTheme.darkTheme,
+//       themeMode: ThemeMode.system, // auto switch
+//       onGenerateRoute: AppRoutes.generateRoute,
+//       initialRoute: AppRoutes.login,
+//     );
+//   }
+// }
+
+// ---------------------- UNIT TEST / SINGLE PAGE RUN ----------------------
+// Uncomment this section to run any page independently for quick testing
+
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
-import 'features/auth/provider/auth_provider.dart';
-import 'routes/app_routes.dart';
+import './routes/screen_export.dart';
 
 void main() {
   runApp(
-    MultiProvider(
-      providers: [ChangeNotifierProvider(create: (_) => AuthProvider())],
-      child: const MyApp(),
+    const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: LoginScreen(), // Change to test another page manually
     ),
   );
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Outfit Store',
-      onGenerateRoute: AppRoutes.generateRoute,
-      initialRoute: AppRoutes.login,
-    );
-  }
-}
