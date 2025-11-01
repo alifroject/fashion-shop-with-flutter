99ac44641c4719f22c0ecb09cbeca0b632b19629
Alifnewjourney Jovani safik <furqonseli@gmail.com>
2025-11-01 16:42:56 +0700

    Modify popscope and apply setcurrent index and onbackpress

--- lib/entry_point.dart
+++ lib/entry_point.dart
@@ -1,21 +1,33 @@
 import 'package:flutter/material.dart';
 import '../../routes/screen_export.dart';
+import 'package:provider/provider.dart';
 
 class EntryPoint extends StatefulWidget {
-  const EntryPoint({super.key});
+  final int initialTab;
+  const EntryPoint({super.key, this.initialTab = 0});
 
   @override
   State<EntryPoint> createState() => _EntryPointState();
 }
 
 class _EntryPointState extends State<EntryPoint> {
-  int _currentIndex = 0;
+  late NavigationProvider navProvider;
+
+  @override
+  void initState() {
+    super.initState();
+
+    navProvider = Provider.of<NavigationProvider>(context, listen: false);
+    navProvider.init(widget.initialTab);
+  }
 
   @override
   Widget build(BuildContext context) {
     final theme = Theme.of(context);
     final screenWidth = Responsive.screenWidth(context);
     final responsiveFont = screenWidth * 0.055;
+
+    navProvider = Provider.of<NavigationProvider>(context);
 
     final List<Widget> pages = [
       HomeScreen(),
@@ -51,41 +63,52 @@ class _EntryPointState extends State<EntryPoint> {
       ),
     ];
 
-    return Scaffold(
-      backgroundColor: theme.scaffoldBackgroundColor,
-      body: AnimatedSwitcher(
-        duration: const Duration(milliseconds: 300),
-        child: pages[_currentIndex],
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-        currentIndex: _currentIndex,
-        onTap: (index) => setState(() => _currentIndex = index),
-        type: BottomNavigationBarType.fixed,
-        selectedItemColor: theme.colorScheme.primary,
-        unselectedItemColor: Colors.grey,
-        backgroundColor: theme.cardColor,
-        elevation: 10,
-        iconSize: screenWidth * 0.07,
-        selectedFontSize: screenWidth * 0.035,
-        unselectedFontSize: screenWidth * 0.03,
-        items: const [
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home_rounded),
-            label: 'Home',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.shopping_cart_rounded),
-            label: 'Cart',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.favorite_rounded),
-            label: 'Favorites',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.person_rounded),
-            label: 'Profile',
-          ),
-        ],
+    return PopScope(
+      canPop: false,
+      onPopInvokedWithResult: (didPop, result) {
+        if (!didPop) {
+          final shouldExit = navProvider.onBackPressed();
+          if (shouldExit) {
+            Navigator.of(context).maybePop();
+          }
+        }
+      },
+      child: Scaffold(
+        backgroundColor: theme.scaffoldBackgroundColor,
+        body: AnimatedSwitcher(
+          duration: const Duration(milliseconds: 300),
+          child: pages[navProvider.currentIndex],
+        ),
+        bottomNavigationBar: BottomNavigationBar(
+          currentIndex: navProvider.currentIndex,
+          onTap: (index) => navProvider.setCurrentIndex(index),
+          type: BottomNavigationBarType.fixed,
+          selectedItemColor: theme.colorScheme.primary,
+          unselectedItemColor: Colors.grey,
+          backgroundColor: theme.cardColor,
+          elevation: 10,
+          iconSize: screenWidth * 0.07,
+          selectedFontSize: screenWidth * 0.035,
+          unselectedFontSize: screenWidth * 0.03,
+          items: const [
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home_rounded),
+              label: 'Home',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.shopping_cart_rounded),
+              label: 'Cart',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.favorite_rounded),
+              label: 'Favorites',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.person_rounded),
+              label: 'Profile',
+            ),
+          ],
+        ),
       ),
     );
   }
