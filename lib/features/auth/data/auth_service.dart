da0018769907c14c6a3e4ee9671f4af626cb6f70
Alifnewjourney Jovani safik <furqonseli@gmail.com>
2025-11-09 09:00:22 +0700

    validate clear auth and fix data response

--- lib/features/auth/data/auth_service.dart
+++ lib/features/auth/data/auth_service.dart
@@ -2,6 +2,8 @@ import 'package:outfit_store/models/user_model.dart'; //user model for User
 import '../../../../../services/api_client.dart';
 import '../../../../../services/storage_service.dart';
 
+//handle authentication logic such as login, signup, logout, and get profile
+
 class AuthService {
   final ApiClient _appClient = ApiClient();
   final StorageService _storage = StorageService();
@@ -13,31 +15,33 @@ class AuthService {
     String email,
     String password,
   ) async {
-    final response = await _appClient.post('/api/auth/signup', {
+    final response = await _appClient.post('/signup', {
       'firstName': firstName,
       'lastName': lastName,
       'email': email,
       'password': password,
     });
-    print('Signup Response: ${response.data}'); 
+    print('Signup Response: ${response.data}');
     final userJson = response.data;
     return User.fromJson(userJson);
   }
 
   //Login
   Future<User> login(String email, String password) async {
-    final response = await _appClient.post("/api/auth/login", {
+    final response = await _appClient.post("/login", {
       'email': email,
       'password': password,
     });
 
-    print(response.data);
-    final data = response.data;
-    final token = data?.toString();
-    final userData = data as Map<String, dynamic>?;
+    final data = response.data as Map<String, dynamic>;
+    final token = data['token'] as String?;
+    final userData = data['user'] as Map<String, dynamic>?;
+
     if (token == null || userData == null) {
       throw Exception("Invalid response format from server.");
-    } 
+    }
+
+    print(response.data);
     await _storage.saveToken(token); //get token
     return User.fromJson(userData);
   }
@@ -47,12 +51,13 @@ class AuthService {
     final token = await _storage.getToken();
     if (token == null) return null;
 
-    final response = await _appClient.get('/api/auth/profile');
+    final response = await _appClient.get('/api/auth/profile', auth: true);
     return User.fromJson(response.data);
   }
 
   //Logout
   Future<void> logout() async {
     await _storage.clearToken();
+    _appClient.clearAuthHeader();
   }
 }
