42290922057e673d0569f86c18fe7f30b0b3f0c1
Alifnewjourney Jovani safik <furqonseli@gmail.com>
2025-11-09 10:39:19 +0700

    Apply auth guard

--- lib/routes/router.dart
+++ lib/routes/router.dart
@@ -1,23 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:outfit_store/routes/route_constants.dart';
 import 'screen_export.dart';
-import '../../services/storage_service.dart';
-import './route_constants.dart';
+
+import './route_guard.dart';
 
 class AppRoutes {
   static const login = '/login';
   static const signup = '/signup';
-  
+  static const entryPointScreenRoute = '/entry_point';
+
   static Route<dynamic> generateRoute(RouteSettings settings) {
     switch (settings.name) {
       case login:
         return MaterialPageRoute(builder: (_) => const LoginScreen());
+
       case signup:
         return MaterialPageRoute(builder: (_) => const SignupScreen());
 
       case entryPointScreenRoute:
-        return MaterialPageRoute(
-          builder: (_) => const OnboardingScreen(),
-        );
+        return MaterialPageRoute(builder: (_) => AuthGuard(child: EntryPoint()));
 
       default:
         return MaterialPageRoute(
@@ -27,4 +28,3 @@ class AppRoutes {
     }
   }
 }
-
